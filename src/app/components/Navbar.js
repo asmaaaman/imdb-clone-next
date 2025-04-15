63054d045f768150e3d758b9a7c7826c74390e30
asmaaaman <asmaa.aman91@gmail.com>
2025-04-15 22:10:36 +0200

    fix issues3

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 import NavbarItem from "./NavbarItem";
 
 const Navbar = () => {
   return (
-    <div
-      className="flex items-center justify-center bg-amber-100 dark:bg-gray-600
+    <Suspense fallback={<div>Loading navbar...</div>}>
+      <div
+        className="flex items-center justify-center bg-amber-100 dark:bg-gray-600
      transition-colors duration-300 gap-6 p-4"
-    >
-      <NavbarItem title="Trending" param="trending" />
-      <NavbarItem title="Top Rated" param="top-rated" />
-    </div>
+      >
+        <NavbarItem title="Trending" param="trending" />
+        <NavbarItem title="Top Rated" param="top-rated" />
+      </div>
+    </Suspense>
   );
 };
 
